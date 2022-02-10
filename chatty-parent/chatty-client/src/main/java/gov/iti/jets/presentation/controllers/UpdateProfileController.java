a33dbb01237c40024d04786069ea190d9b7e87b6
Salma Fayez <salma.fayez.himida@gmail.com>
2022-02-10 20:31:25 +0200

    update profile view and controller complete

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/UpdateProfileController.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/UpdateProfileController.java
@@ -1,4 +1,52 @@
 package gov.iti.jets.presentation.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
 public class UpdateProfileController {
+
+    @FXML
+    private TextField bioTextField;
+
+    @FXML
+    private DatePicker birthDateDatePicker;
+
+    @FXML
+    private ComboBox<?> countryComboBox;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private ComboBox<?> genderComboBox;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private Circle profilePictureCirclr;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private Hyperlink uploadPictureHyperLink;
+
+    @FXML
+    void onUpdateButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUploadPictureHyperLinkAction(ActionEvent event) {
+
+    }
+
 }
+
