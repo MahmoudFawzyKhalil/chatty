fefa7eba9de12061b5b15b41d484dbfb099c9eb4
hafsamohamed <hafsamohmed0@gmail.com>
2022-02-10 20:40:20 +0000

    Write invitation fxml css and controller

---
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/InvitationController.java
@@ -0,0 +1,18 @@
+package gov.iti.jets.presentation.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+public class InvitationController {
+    @FXML
+    private Label friendNameLabel;
+
+    @FXML
+    private Label friendPhoneNumberLabel;
+
+    @FXML
+    private Button onAcceptButtonAction;
+
+    @FXML
+    private Button onRefuseButtonAction;
+}
