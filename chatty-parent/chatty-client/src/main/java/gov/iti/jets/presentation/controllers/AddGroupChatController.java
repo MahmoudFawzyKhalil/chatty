6db7c0d86348b0050d7bef6dfbeaba87f9609cca
Salma Fayez <salma.fayez.himida@gmail.com>
2022-02-10 21:43:23 +0200

    add group chat view one and two add group chat controller
    ps: check again the cancel hyperlink
    ps: the size of checkbox text

---
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/AddGroupChatController.java
@@ -0,0 +1,63 @@
+package gov.iti.jets.presentation.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddGroupChatController implements Initializable {
+
+    @FXML
+    private Hyperlink cancelHyperLink;
+
+    @FXML
+    private CheckBox contactNameTextField;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private TextField groupNameTextField;
+
+    @FXML
+    private Circle groupPictureCircle;
+
+    @FXML
+    private Hyperlink uploadPictureHyperLink;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    void onCancelHyperLinkClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNextButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCreateButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUploadPictureHyperLinkAction(ActionEvent event) {
+
+    }
+}
