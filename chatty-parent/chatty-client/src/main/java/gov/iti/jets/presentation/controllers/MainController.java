a9a838a6a2ecf77a4bb0c8a4eab5bf77db50ee7d
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-02-11 23:40:13 +0200

    Testing out ListView and Observable Lists and List Properties.

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/MainController.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/MainController.java
@@ -2,15 +2,23 @@ package gov.iti.jets.presentation.controllers;
 
 import gov.iti.jets.presentation.customcontrols.ContactChatMenuItem;
 import gov.iti.jets.presentation.customcontrols.GroupChatMenuItem;
+import gov.iti.jets.presentation.customcontrols.GroupMemberItem;
 import gov.iti.jets.presentation.util.PaneCoordinator;
 import gov.iti.jets.presentation.util.StageCoordinator;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,10 +41,21 @@ public class MainController implements Initializable {
     private Circle userStatusCircle;
 
 
+    @FXML
+    private ListView<GroupChatMenuItem> groupsListView;
+    private static ObservableList<GroupChatMenuItem> observableList;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        createAndAddGroupChatMenuItem();
+//        createAndAddGroupChatMenuItem();
         createAndAddContactChatMenuItem();
+        groupsListView.setCellFactory(new GroupMemberCellFactory());
+        observableList = FXCollections.<GroupChatMenuItem>observableArrayList();
+        observableList.add(new GroupChatMenuItem());
+        SimpleListProperty<GroupChatMenuItem> slp = new SimpleListProperty<>(observableList);
+        groupsListView.itemsProperty().bind(slp);
+        slp.add(new GroupChatMenuItem());
     }
 
     @FXML
@@ -66,7 +85,7 @@ public class MainController implements Initializable {
 
     @FXML
     void onChatBotButtonAction(ActionEvent event) {
-
+        observableList.add(new GroupChatMenuItem());
     }
 
     @FXML
@@ -98,5 +117,27 @@ public class MainController implements Initializable {
         var item = new GroupChatMenuItem();
         item.setOnMouseClicked(e -> paneCoordinator.switchToChatPane());
         groupChatsVbox.getChildren().add(item);
+    }
+
+    private static class GroupMemberCellFactory implements Callback<ListView<GroupChatMenuItem>, ListCell<GroupChatMenuItem>> {
+        @Override
+        public ListCell<GroupChatMenuItem> call(ListView<GroupChatMenuItem> param) {
+            return new ListCell<>(){
+                @Override
+                public void updateItem(GroupChatMenuItem person, boolean empty) {
+                    super.updateItem(person, empty);
+                    if (empty) {
+                        setText(null);
+                        setGraphic(null);
+                    } else if (person != null) {
+                        setText(null);
+                        setGraphic(new GroupChatMenuItem());
+                    } else {
+                        setText("null");
+                        setGraphic(null);
+                    }
+                }
+            };
+        }
     }
 }
