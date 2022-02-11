62556a6ead7420308c2ef0927084ca9cf0f91876
christinenedaa <christinenedaa@gmail.com>
2022-02-12 00:41:29 +0200

    create user status model

---
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/models/UserStatusModel.java
@@ -0,0 +1,39 @@
+package gov.iti.jets.presentation.models;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class UserStatusModel {
+    private IntegerProperty userStatusId = new SimpleIntegerProperty();
+    private StringProperty userStatusName = new SimpleStringProperty();
+
+    public UserStatusModel(int StatusId, String StatusName){
+        this.userStatusId = new SimpleIntegerProperty(StatusId);
+        this.userStatusName = new SimpleStringProperty(StatusName);
+    }
+    public String getUserStatusName() {
+        return userStatusName.get();
+    }
+
+    public StringProperty userStatusNameProperty() {
+        return userStatusName;
+    }
+
+    public void setUserStatusName(String userStatusName) {
+        this.userStatusName.set(userStatusName);
+    }
+
+    public int getUserStatusId() {
+        return userStatusId.get();
+    }
+
+    public IntegerProperty userStatusIdProperty() {
+        return userStatusId;
+    }
+
+    public void setUserStatusId(int userStatusId) {
+        this.userStatusId.set(userStatusId);
+    }
+}
