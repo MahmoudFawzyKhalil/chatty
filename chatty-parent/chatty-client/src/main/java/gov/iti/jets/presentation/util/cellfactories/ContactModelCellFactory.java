18421806047a33e2f89cd1e7f7696620b3d07526
christinenedaa <christinenedaa@gmail.com>
2022-02-12 20:50:02 +0200

    create contact model cell factory

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/cellfactories/ContactModelCellFactory.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/cellfactories/ContactModelCellFactory.java
@@ -1,4 +1,34 @@
 package gov.iti.jets.presentation.util.cellfactories;
 
-public class ContactModelCellFactory {
-}
+import gov.iti.jets.presentation.customcontrols.ContactChatMenuItem;
+import gov.iti.jets.presentation.models.ContactModel;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+public class ContactModelCellFactory implements Callback<ListView<ContactModel>, ListCell<ContactModel>> {
+        @Override
+        public ListCell<ContactModel> call(ListView<ContactModel> param) {
+            ListCell<ContactModel> cell = new ListCell<>(){
+                @Override
+                public void updateItem(ContactModel contact, boolean empty) {
+                    super.updateItem(contact, empty);
+                    if (empty) {
+                        setText(null);
+                        setGraphic(null);
+                    } else if (contact != null) {
+                        setText(null);
+                        setGraphic(new ContactChatMenuItem()); //contact argument
+                    } else {
+                        setText("null");
+                        setGraphic(null);
+                    }
+                }
+            };
+
+            cell.setOnMouseClicked(e -> System.out.println(cell.getItem()));
+
+            return cell;
+        }
+    }
+
