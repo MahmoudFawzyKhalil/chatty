16af5dc2ed05abe81bb20eb06c34bea0126cc4f6
hafsamohamed <hafsamohmed0@gmail.com>
2022-02-12 20:44:41 +0000

    Create the GroupMemberModelCellFactory

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/cellfactories/GroupMemberModelCellFactory.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/cellfactories/GroupMemberModelCellFactory.java
@@ -1,4 +1,37 @@
 package gov.iti.jets.presentation.util.cellfactories;
 
+
+import gov.iti.jets.presentation.customcontrols.GroupChatMenuItem;
+import gov.iti.jets.presentation.models.GroupChatModel;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
 public class GroupMemberModelCellFactory {
+
+    public class GroupMemberCellFactory implements Callback<ListView<GroupChatModel>, ListCell<GroupChatModel>> {
+        @Override
+        public ListCell<GroupChatModel> call(ListView<GroupChatModel> param) {
+            ListCell<GroupChatModel> cell = new ListCell<>(){
+                @Override
+                public void updateItem(GroupChatModel groupChat, boolean empty) {
+                    super.updateItem(groupChat, empty);
+                    if (empty) {
+                        setText(null);
+                        setGraphic(null);
+                    } else if (groupChat != null) {
+                        setText(null);
+                        setGraphic(new GroupChatMenuItem());
+                    } else {
+                        setText("null");
+                        setGraphic(null);
+                    }
+                }
+            };
+
+            cell.setOnMouseClicked(e -> System.out.println(cell.getItem()));
+            return cell;
+        }
+    }
+
 }
