83679a54fcccfcb2e7d9a788a39892b54dd265a7
AhmedOsama <ahmedosamam03@gmail.com>
2022-02-12 20:56:56 +0200

    change to list view

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/AddContactController.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/AddContactController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -21,11 +22,11 @@ public class AddContactController implements Initializable {
     private TextField phoneNumberTextField;
 
     @FXML
-    private VBox contactsVBox;
+    private ListView contactsListView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        contactsVBox.getChildren().add(new AddContactTextField());
+//        contactsVBox.getChildren().add(new AddContactTextField());
     }
 
     @FXML
