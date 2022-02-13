2563a3f1835388f07a3dbcb9c9020f4279faa434
AhmedOsama <ahmedosamam03@gmail.com>
2022-02-13 19:15:24 +0200

    Add ChatBubbleCellFactory , extractor for MessageModel

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/cellfactories/ChatBubbleCellFactory.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/cellfactories/ChatBubbleCellFactory.java
@@ -1,4 +1,38 @@
 package gov.iti.jets.presentation.util.cellfactories;
 
-public class ChatBubbleCellFactory {
+import gov.iti.jets.presentation.customcontrols.ReceivedChatBubble;
+import gov.iti.jets.presentation.customcontrols.SentChatBubble;
+import gov.iti.jets.presentation.models.MessageModel;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+public class ChatBubbleCellFactory implements Callback<ListView<MessageModel>, ListCell<MessageModel>> {
+
+    @Override
+    public ListCell<MessageModel> call(ListView<MessageModel> param) {
+        ListCell<MessageModel> cell = new ListCell<>() {
+            @Override
+            public void updateItem(MessageModel message, boolean empty) {
+                super.updateItem(message, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else if (message != null) {
+                    setText(null);
+                    if (message.isSentByMe()) {
+                        setGraphic(new SentChatBubble());
+                    } else {
+                        setGraphic(new ReceivedChatBubble());
+                    }
+                } else {
+                    setText("null");
+                    setGraphic(null);
+                }
+            }
+        };
+
+        cell.setOnMouseClicked(e -> System.out.println(cell.getItem()));
+        return cell;
+    }
 }
