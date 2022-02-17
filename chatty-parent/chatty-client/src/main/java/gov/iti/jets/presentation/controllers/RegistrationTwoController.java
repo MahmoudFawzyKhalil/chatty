8bfc40eb1c2515f85c7c985e4d05011e7873a9d3
AhmedOsama <ahmedosamam03@gmail.com>
2022-02-17 02:24:42 +0200

    change countryChoiceBox<String> to <CountryModel>

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/RegistrationTwoController.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/RegistrationTwoController.java
@@ -1,14 +1,18 @@
 package gov.iti.jets.presentation.controllers;
 
+import gov.iti.jets.presentation.models.CountryModel;
 import gov.iti.jets.presentation.models.RegisterModel;
 import gov.iti.jets.presentation.util.ModelFactory;
 import gov.iti.jets.presentation.util.StageCoordinator;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,7 +29,7 @@ public class RegistrationTwoController implements Initializable {
     private DatePicker birthDateDatePicker;
 
     @FXML
-    private ChoiceBox<String> countryChoiceBox;
+    private ChoiceBox<CountryModel> countryChoiceBox;
 
     @FXML
     private TextField emailTextField;
@@ -35,12 +39,18 @@ public class RegistrationTwoController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<CountryModel> countryModels = FXCollections.observableArrayList();
+        CountryModel countryModel = new CountryModel(1, "egy");
+        countryModels.add(countryModel);
+        setCountryChoiceBoxConverter();
+        countryChoiceBox.setItems(countryModels);
         emailTextField.textProperty().bindBidirectional(registerModel.emailProperty());
-        countryChoiceBox.valueProperty().bindBidirectional(registerModel.getCountry().countryNameProperty());
+        countryChoiceBox.valueProperty().bindBidirectional(registerModel.countryProperty());
         genderChoiceBox.valueProperty().bindBidirectional(registerModel.genderProperty());
         birthDateDatePicker.valueProperty().bindBidirectional(registerModel.birthDateProperty());
         bioTextField.textProperty().bindBidirectional(registerModel.bioProperty());
     }
+
     @FXML
     void onPreviousButtonAction(ActionEvent event) {
         stageCoordinator.switchToRegisterSceneOne();
@@ -49,5 +59,23 @@ public class RegistrationTwoController implements Initializable {
     @FXML
     void onNextButtonAction(ActionEvent event) {
         stageCoordinator.switchToRegisterSceneThree();
+    }
+
+    private void setCountryChoiceBoxConverter() {
+        countryChoiceBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(CountryModel country) {
+                if (country == null) {
+                    return null;
+                } else {
+                    return country.getCountryName();
+                }
+            }
+
+            @Override
+            public CountryModel fromString(String id) {
+                return null;
+            }
+        });
     }
 }
