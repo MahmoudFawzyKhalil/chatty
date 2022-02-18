49558574179c427fc41c69886c2280ab53396e57
MahmoudFawzyKhalil <73137611+MahmoudFawzyKhalil@users.noreply.github.com>
2022-02-18 03:03:23 +0200

    Update invitations table to use sender and receiver

--- chatty.sql
+++ chatty.sql
@@ -39,8 +39,8 @@ CREATE TABLE contacts(
     contactee_phone_number VARCHAR(50) NOT NULL);
 
 CREATE TABLE invitations(
-	contact_phone_number VARCHAR(50) NOT NULL,
-    contactee_phone_number VARCHAR(50) NOT NULL);
+	sender VARCHAR(50) NOT NULL,
+    	receiver VARCHAR(50) NOT NULL);
     
 CREATE TABLE group_chats_users(
 	group_chat_id INT NOT NULL,
@@ -61,10 +61,10 @@ ALTER TABLE contacts
     ADD CONSTRAINT CHECK (contact_phone_number != contactee_phone_number);
 
 ALTER TABLE invitations
-    ADD CONSTRAINT FOREIGN KEY(contact_phone_number) REFERENCES users(phone_number),
-    ADD CONSTRAINT FOREIGN KEY(contactee_phone_number) REFERENCES users(phone_number),
-    ADD CONSTRAINT PRIMARY KEY(contact_phone_number, contactee_phone_number),
-    ADD CONSTRAINT CHECK (contact_phone_number != contactee_phone_number);
+    ADD CONSTRAINT FOREIGN KEY(sender) REFERENCES users(phone_number),
+    ADD CONSTRAINT FOREIGN KEY(receiver) REFERENCES users(phone_number),
+    ADD CONSTRAINT PRIMARY KEY(sender, receiver),
+    ADD CONSTRAINT CHECK (sender != receiver);
     
 ALTER TABLE group_chats_users
     ADD CONSTRAINT FOREIGN KEY(group_chat_id) REFERENCES group_chats(group_chat_id),
@@ -340,7 +340,7 @@ INSERT INTO `chatty`.`group_chats_users` (`group_chat_id`, `user_phone_number`) 
 INSERT INTO `chatty`.`group_chats_users` (`group_chat_id`, `user_phone_number`) VALUES ('2', '33333333333');
 INSERT INTO `chatty`.`group_chats_users` (`group_chat_id`, `user_phone_number`) VALUES ('2', '44444444444');
 
-INSERT INTO `chatty`.`invitations` (`contact_phone_number`, `contactee_phone_number`) VALUES ('11111111111', '33333333333');
+INSERT INTO `chatty`.`invitations` (`sender`, `receiver`) VALUES ('11111111111', '33333333333');
 
 
 
