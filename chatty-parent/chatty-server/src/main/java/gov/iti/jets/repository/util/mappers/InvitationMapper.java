d1aff58c8cd6d5531491e4f3ce5e07b3305b9ead
Salma Fayez <salma.fayez.himida@gmail.com>
2022-02-18 17:25:07 +0200

    update invitation mapper

--- chatty-parent/chatty-server/src/main/java/gov/iti/jets/repository/util/mappers/InvitationMapper.java
+++ chatty-parent/chatty-server/src/main/java/gov/iti/jets/repository/util/mappers/InvitationMapper.java
@@ -3,12 +3,16 @@ package gov.iti.jets.repository.util.mappers;
 import gov.iti.jets.commons.dtos.InvitationDto;
 import gov.iti.jets.repository.entities.InvitationEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = {ContactMapper.class})
 public interface InvitationMapper {
     InvitationMapper INSTANCE = Mappers.getMapper(InvitationMapper.class);
 
+    @Mapping( target = "contactEntity", source = "contactDto")
     InvitationEntity invitationDtoToEntity(InvitationDto invitationDto);
+
+    @Mapping( target = "contactDto", source = "contactEntity")
     InvitationDto invitationEntityToDto(InvitationEntity invitationEntity);
 }
