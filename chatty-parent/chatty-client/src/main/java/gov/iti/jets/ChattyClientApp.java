e89c085d32b1e37e859b0fd030d8d28adf6cc4af
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-02-19 10:14:58 +0200

    Test out UDP discovery

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/ChattyClientApp.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/ChattyClientApp.java
@@ -4,9 +4,45 @@ import gov.iti.jets.presentation.util.StageCoordinator;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 
 public class ChattyClientApp extends Application {
     private StageCoordinator stageCoordinator = StageCoordinator.getInstance();
+
+    @Override
+    public void init(){
+        new Thread(() -> {
+            try {
+                DatagramSocket datagramSocket = new DatagramSocket(7777);
+
+                while (true) {
+                    datagramSocket.setBroadcast( true );
+
+                    String message = "HELLO_HELLO";
+
+                    DatagramPacket sentPacket = new DatagramPacket( message.getBytes(), message.getBytes().length );
+
+                    sentPacket.setAddress( InetAddress.getByName( "255.255.255.255" ) );
+                    sentPacket.setPort( 4444 );
+
+                    datagramSocket.send( sentPacket );
+
+                    datagramSocket.receive( sentPacket );
+
+                    System.err.println(new String(sentPacket.getData()).trim());
+
+                    Thread.sleep( 1000 );
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
 
     @Override
     public void start( Stage primaryStage ) {
--- chatty-parent/chatty-server/src/main/java/gov/iti/jets/ChattyServerApp.java
+++ chatty-parent/chatty-server/src/main/java/gov/iti/jets/ChattyServerApp.java
@@ -5,10 +5,47 @@ import gov.iti.jets.presentation.util.StageCoordinator;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.*;
+
 
 public class ChattyServerApp extends Application {
     private StageCoordinator stageCoordinator = StageCoordinator.getInstance();
     private RmiManager rmiManager = RmiManager.getInstance();
+
+    @Override
+    public void init(){
+        new Thread(() -> {
+            try {
+                DatagramSocket datagramSocket = new DatagramSocket(4444);
+
+                while (true) {
+
+                    DatagramPacket receivedPacket = new DatagramPacket( new byte[2048], 2048 );
+
+                    datagramSocket.receive( receivedPacket );
+
+                    System.err.println(new String(receivedPacket.getData()).trim());
+
+                    DatagramSocket responderSocket = new DatagramSocket();
+
+                    String serverIp = InetAddress.getLocalHost().getHostAddress();
+                    DatagramPacket responderPacket = new DatagramPacket( serverIp.getBytes(),
+                            serverIp.getBytes().length, receivedPacket.getSocketAddress() );
+
+                    System.err.println(receivedPacket.getSocketAddress());
+
+//                    NetworkInterface.getNetworkInterfaces()
+
+                    responderSocket.send( responderPacket );
+
+                    Thread.sleep( 1000 );
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
     @Override
     public void start(Stage primaryStage){
