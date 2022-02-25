738c9c4bdd73a890368c66031afdd531c16b3d50
AhmedOsama <ahmedosamam03@gmail.com>
2022-02-25 16:04:27 +0200

    Fixed update profile remember

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/LoginController.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/controllers/LoginController.java
@@ -139,7 +139,7 @@ public class LoginController implements Initializable {
                                 });
                             });
                 });
-
+                ModelFactory.getInstance().getUpdateProfileModel().resetData();
                 stageCoordinator.switchToMainScene();
 
             } else {
