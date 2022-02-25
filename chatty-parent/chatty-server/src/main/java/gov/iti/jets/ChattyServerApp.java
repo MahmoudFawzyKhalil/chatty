b24def350bfd80818f068bc56b0df5e26d92e53c
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-02-25 14:18:25 +0200

    Add method call to notify users of shutdown in overriden stop method of server

--- chatty-parent/chatty-server/src/main/java/gov/iti/jets/ChattyServerApp.java
+++ chatty-parent/chatty-server/src/main/java/gov/iti/jets/ChattyServerApp.java
@@ -3,6 +3,8 @@ package gov.iti.jets;
 import gov.iti.jets.network.RmiManager;
 import gov.iti.jets.presentation.util.StageCoordinator;
 import gov.iti.jets.repository.util.ConnectionPool;
+import gov.iti.jets.services.ServerNotificationsService;
+import gov.iti.jets.services.util.ServiceFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -10,6 +12,7 @@ import javafx.stage.Stage;
 public class ChattyServerApp extends Application {
     private StageCoordinator stageCoordinator = StageCoordinator.getInstance();
     private RmiManager rmiManager = RmiManager.getInstance();
+    private ServerNotificationsService serverNotificationsService = ServiceFactory.getInstance().getServerNotificationsService();
 
     @Override
     public void start(Stage primaryStage) {
@@ -24,6 +27,7 @@ public class ChattyServerApp extends Application {
     @Override
     public void stop() throws Exception {
         ConnectionPool.cleanup();
+        serverNotificationsService.notifyClientsOfServerShutDown();
         rmiManager.stop();
     }
 
