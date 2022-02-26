1b37810dec1de2e0bda13cae5f2992df62f0a585
AhmedOsama <ahmedosamam03@gmail.com>
2022-02-26 20:32:01 +0200

    Validate register phonenumber to egy numbers

--- chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/UiValidator.java
+++ chatty-parent/chatty-client/src/main/java/gov/iti/jets/presentation/util/UiValidator.java
@@ -18,6 +18,7 @@ public class UiValidator {
     private static final UiValidator INSTANCE = new UiValidator();
 
     public static Pattern PHONE_NUMBER_PATTERN = Pattern.compile( "[0-9]{11}" );
+    public static Pattern EGY_PHONE_NUMBER_PATTERN = Pattern.compile( "^(01)[1250][0-9]{8}$" );
     public static Pattern USER_NAME_PATTERN = Pattern.compile( "[a-zA-Z0-9]+" );
     public static Pattern GROUP_CHAT_NAME_PATTERN = Pattern.compile( "[a-zA-Z0-9]+" );
     public static Pattern EMAIL_PATTERN = Pattern.compile( "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$" );
